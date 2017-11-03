d6be2a8c9347742ea3aaa120416ccdd11d3dabb0
Luiz Claudio <luizcns18@gmail.com>
2017-11-03 18:37:48 -0300

    refactor: Add destructuring

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,25 +1,31 @@
-// import React, { Component } from 'react'
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+// import React from 'react'
 import Book from './Book'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
-const BookShelf = (props) => (
-    <div className="bookshelf">
-        <h2 className="bookshelf-title">{props.shelfTitle}</h2>
-        <div className="bookshelf-books">
-            <ol className="books-grid">
-                {props.books.map((book) => (
-                    <li key={props.books.indexOf(book)}>
-                        <Book book={book} />
-                    </li>
-                ))}
-            </ol>
+class BookShelf extends Component {
+    static proptypes = {
+        books: PropTypes.array.isRequired
+    }
+
+    render(){
+        const { shelfTitle, books } = this.props;
+
+        return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelfTitle}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map((book) => (
+                        <li key={books.indexOf(book)}>
+                            <Book book={book} />
+                        </li>
+                    ))}
+                </ol>
+            </div>
         </div>
-    </div>
-);
-
-BookShelf.propTypes = {
-    books: PropTypes.array.isRequired
+        )
+    }
 }
 
 export default BookShelf
