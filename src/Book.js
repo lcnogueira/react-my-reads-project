203110612d9af28715b6605937a0aa4348ad6df4
Luiz Claudio <luizcns18@gmail.com>
2017-11-03 17:42:33 -0300

    feat: Add proptypes into book and bookshelf components

--- src/Book.js
+++ src/Book.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const Book = (props) => (
-    <div className="book">
-        <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-            <div className="book-shelf-changer">
-                <select>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
+class Book extends Component {
+
+    render () {
+        return (
+            <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                <div className="book-shelf-changer">
+                    <select>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div className="book-title">{props.book.title}</div>
-        <div className="book-authors">{props.book.author}</div>
-    </div >
-);
+            <div className="book-title">{this.props.book.title}</div>
+            <div className="book-authors">{this.props.book.author}</div>
+        </div >
+        )
+    }
+}
+
+
+Book.PropTypes = {
+    book: PropTypes.object.isRequired,
+}
 
 export default Book
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
-class BookShelf extends Component {
+const BookShelf = (props) => (
+    <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.shelfTitle}</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                {props.books.map((book) => (
+                    <li key={props.books.indexOf(book)}>
+                        <Book book={book} />
+                    </li>
+                ))}
+            </ol>
+        </div>
+    </div>
+);
 
-    render() {
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books.map((book) => (
-                            <li key={this.props.books.indexOf(book)}>
-                                <Book book={book} />
-                            </li>
-                        ))}
-                    </ol>
-                </div>
-            </div>
-        )
-
-    }
-
+BookShelf.propTypes = {
+    books: PropTypes.array.isRequired
 }
 
 export default BookShelf
