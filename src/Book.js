595bacd79d63c4dc1a4bc5bc823d8318ef760981
Luiz Claudio <luizcns18@gmail.com>
2017-11-04 09:58:42 -0300

    fix: Show authors names

--- src/Book.js
+++ src/Book.js
@@ -27,7 +27,7 @@ class Book extends Component {
                 </div>
             </div>
             <div className="book-title">{book.title}</div>
-            <div className="book-authors">{book.author}</div>
+            <div className="book-authors">{book.authors}</div>
         </div >
         )
     }
