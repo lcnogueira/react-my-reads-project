4678e7abf2069e98de8773fd9398b5bbdd8ff7d8
Luiz Claudio <luizcns18@gmail.com>
2017-11-05 09:55:46 -0300

    feat: Add debounce to input on search component

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import Book from './Book'
+import { DebounceInput } from 'react-debounce-input'
 
 class Search extends Component {
 
+    state = {
+        books: []
+    }
+
+    searchBooks = (query) => {
+        if(query){
+            BooksAPI.search(query,1000).then( books => {
+                this.setState({books: books})
+            })
+        }else{
+            this.setState({books:[]})
+        }
+    }
+
     render() {
+
+        const {onChangeShelf} = this.props
+        const {books} = this.state
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -12,11 +33,23 @@ class Search extends Component {
                         className="close-search"
                     >Close></Link>
                     <div className="search-books-input-wrapper">
-                        <input type="text" placeholder="Search by title or author" />
+                        <DebounceInput 
+                            debounceTimeout={100}
+                            onChange={event => this.searchBooks(event.target.value)}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {books.map((book) => (
+                            <li key={book.id}>
+                                <Book
+                                    book={book}
+                                    onChangeShelf={onChangeShelf}
+                                />
+                            </li>
+                        ))}
+                    </ol>
                 </div>
             </div>
         )
